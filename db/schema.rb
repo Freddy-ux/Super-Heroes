1f23ffe2f98e1f2e0479e6bb52f930bffb05561c
Freddy <alfredmuleri569@gmail.com>
2023-07-11 22:19:08 +0300

    Done with Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_180719) do
+  create_table "hero_powers", force: :cascade do |t|
+    t.string "strength"
+    t.integer "hero_id", null: false
+    t.integer "power_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hero_id"], name: "index_hero_powers_on_hero_id"
+    t.index ["power_id"], name: "index_hero_powers_on_power_id"
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "hero_powers", "heros"
+  add_foreign_key "hero_powers", "powers"
 end
